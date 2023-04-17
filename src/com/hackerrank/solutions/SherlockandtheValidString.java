aa27440c309e4f3054155afc270bbb763511ef74
devangadobe <devangadobe@gmail.com>
2023-04-17 08:33:58 +0530

    mixed solutions hrnk and ltcd

---
+++ src/com/hackerrank/solutions/SherlockandtheValidString.java
@@ -0,0 +1,60 @@
+package com.hackerrank.solutions;
+
+import java.util.*;
+
+public class SherlockandtheValidString {
+
+	public static void main(String[] args) {
+
+		System.out.println(SherlockandtheValidString.isValid("aaaaabc"));
+	}
+
+	public static String isValid(String s) {
+		final String yes = "YES";
+		final String no = "NO";
+		Map<Character, Integer> hmap = new HashMap<Character, Integer>();
+
+		for (char c : s.toCharArray()) {
+			if (hmap.containsKey(c)) {
+				hmap.put(c, hmap.get(c) + 1);
+			} else {
+				hmap.put(c, 1);
+			}
+		}
+
+		Set<Integer> hset = new HashSet<Integer>(hmap.values());
+		if (hset.size() > 2) {
+			return no;
+		}
+		if (hset.size() < 2) {
+			return yes;
+		}
+		List<Integer> list = new ArrayList<Integer>(hset);
+
+		if(!hset.contains(1) && Math.abs(list.get(0)-list.get(1))!=1){
+			return no;
+		}
+		
+		
+		Map<Integer, Integer> imap = new HashMap<Integer, Integer>();
+		for(int i : hmap.values()) {
+			if (imap.containsKey(i)) {
+				imap.put(i, imap.get(i) + 1);
+			} else {
+				imap.put(i, 1);
+			}
+		}
+		
+		if(imap.containsKey(1) && imap.get(1) > 1){
+			return no;
+		}
+		
+		if(imap.containsValue(1)) {
+            return yes;
+        }
+		
+		
+		return no;
+	}
+
+}
--- src/com/leetcode/NextPermutation.java
+++ src/com/leetcode/NextPermutation.java
@@ -1,43 +1,64 @@
 package com.leetcode;
 
-import java.util.*;
-
 public class NextPermutation {
 
 	public static void main(String[] args) {
 		
-		int[] nums = {1,1,5};
+		int[] nums = {1,3,2};
 		
 		new NextPermutation().nextPermutation(nums);
 
 	}
 	
     public void nextPermutation(int[] nums) {
-    	List<String> list = new ArrayList<String>();	
-    	String str="";
-    	for(int i =0; i<nums.length; i++) {
-    		str+=nums[i];
+    	
+    	if(null == nums || nums.length == 0 || nums.length == 1){
+    		return;
     	}
-    	list = permutations(str,"",list);
-    	Collections.sort(list);
-    	int j=0;
-        for(char c : list.get((list.lastIndexOf(str)+1)%list.size()).toCharArray()){
-        	nums[j] = Character.getNumericValue(c);
-        	j++;
-        }
+    	
+    	if(nums.length == 2) {
+    		int temp = nums[0];
+    		nums[0] = nums[1];
+    		nums[1] = temp;
+    		return;
+    	}
+    	
+    	int breakpoint = 0;
+    	int i = nums.length-2;
+    	
+    	while(i>=0 && nums[i]>=nums[i+1]){
+    		i--;
+    	}
+    	
+    	if(i==-1){
+    		nums  = reverse(nums,0,nums.length-1);
+    		return;
+    	}
+    	
+    	breakpoint = i;
+    	i = nums.length-1;
+    	
+    	while(i>0 && nums[i] <= nums[breakpoint] ) {
+    		i--;
+    	}
+    	
+    	int temp = nums[breakpoint];
+    	nums[breakpoint] = nums[i];
+    	nums[i] = temp;
+    	
+    	nums = reverse(nums,breakpoint+1,nums.length-1);   	
+    	   	
     }
 
-	private List<String> permutations(String ros, String ans, List<String> list) {		
-		if(ros.length()==0) {
-			list.add(ans);
+	private int[] reverse(int[] nums,int start, int end) {
+		int temp;
+		for(int i=end,j=start ; i>j ; i--,j++) {
+			temp = nums[i];
+			nums[i] = nums[j];
+			nums[j] = temp;
 		}
-		
-		for(int i=0; i<ros.length(); i++) {
-			list = permutations(ros.substring(0,i)+ros.substring(i+1),
-					            ans+ros.charAt(i),
-					            list);			
-		}		
-		return list;
+		return nums;
 	}
+
 
 }
