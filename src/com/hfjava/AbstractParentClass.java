84e8bc05cb04cf0fd6e08679f6ac99627891c39a
devangadobe <devangadobe@gmail.com>
2023-04-20 00:54:55 +0530

    abstract class behavior

---
+++ src/com/hfjava/AbstractParentClass.java
@@ -0,0 +1,9 @@
+package com.hfjava;
+
+public abstract class AbstractParentClass {
+	
+	public final void getMyName() {
+		System.out.println("Parent abstract class.");
+	}
+
+}
---
+++ src/com/hfjava/ChildClass.java
@@ -0,0 +1,14 @@
+package com.hfjava;
+
+public class ChildClass extends AbstractParentClass{
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Child");
+		
+		new ChildClass().getMyName();
+
+	}
+	
+
+}
