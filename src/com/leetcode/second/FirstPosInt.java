fe2f4751a5420e44b725547928e3aa972645907d
devangadobe <devangadobe@gmail.com>
2023-05-17 10:00:20 +0530

    Update FirstPosInt.java

--- src/com/leetcode/second/FirstPosInt.java
+++ src/com/leetcode/second/FirstPosInt.java
@@ -5,22 +5,22 @@ import java.util.*;
 public class FirstPosInt {
 
 	public static void main(String[] args) {
-		int[] nums = {1,2,0};
-       System.out.println(new FirstPosInt().firstMissingPositive(nums));
+		int[] nums = { 1, 2, 0 };
+		System.out.println(new FirstPosInt().firstMissingPositive(nums));
 	}
 
 	public int firstMissingPositive(int[] nums) {
 		int l = nums.length;
 		int p = 1;
-		Map<Integer,Integer> hmap = new HashMap<Integer, Integer>();
-		for(int i=0; i<l; i++) {
-			hmap.put(nums[i], null);		
+		Map<Integer, Integer> hmap = new HashMap<Integer, Integer>();
+		for (int i = 0; i < l; i++) {
+			hmap.put(nums[i], null);
 		}
-		int i=0;
-		while(i<hmap.size()) {
-			if(hmap.containsKey(p)){
+		int i = 0;
+		while (i < hmap.size()) {
+			if (hmap.containsKey(p)) {
 				p++;
-			}else {
+			} else {
 				break;
 			}
 			i++;
