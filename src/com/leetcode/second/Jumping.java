5ac6532205438e431e45851869c7efbfaeb7adc5
devangadobe <devangadobe@gmail.com>
2023-05-29 11:00:31 +0530

    Update Jumping.java

--- src/com/leetcode/second/Jumping.java
+++ src/com/leetcode/second/Jumping.java
@@ -3,7 +3,7 @@ package com.leetcode.second;
 public class Jumping {
 
 	public static void main(String[] args) {
-		int[] nums = {2,1,1,1};
+		int[] nums = {5,6,4,4,6,9,4,4,7,4,4,8,2,6,8,1,5,9,6,5,2,7,9,7,9,6,9,4,1,6,8,8,4,4,2,0,3,8,5};
 		System.out.println(new Jumping().jump(nums));
 
 	}
