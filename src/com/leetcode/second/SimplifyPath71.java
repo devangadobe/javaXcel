9be268f9ce42f949f8957d6ad1c13437b5417794
devangadobe <devangadobe@gmail.com>
2023-06-14 12:29:47 +0530

    Update SimplifyPath71.java

--- src/com/leetcode/second/SimplifyPath71.java
+++ src/com/leetcode/second/SimplifyPath71.java
@@ -5,12 +5,12 @@ import java.util.Stack;
 public class SimplifyPath71 {
 
 	public static void main(String[] args) {
-		System.out.println(new SimplifyPath71().simplifyPath("/a/./b/../../c/"));
+		System.out.println(new SimplifyPath71().simplifyPath("/..."));
 	}
 
 	public String simplifyPath(String path) {
 		StringBuilder stb = new StringBuilder();
-		Stack<Integer> stk = new Stack<Integer>();
+		Stack<String> stk = new Stack<String>();
 		boolean flag = false;
 		for (int i = path.length() - 1; i >= 0; i--) {
 			if (path.charAt(i) != '/' && path.charAt(i) != '.' && stk.empty()) {
@@ -23,16 +23,31 @@ public class SimplifyPath71 {
 				}
 			} else if (path.charAt(i) == '.') {
 				if (i > 0 && (path.charAt(i - 1) == '.')) {
-					stk.push(i);
+					stk.push("..");
+				}else {
+					stk.push(".");
 				}
 			} else if (path.charAt(i) != '/' && path.charAt(i) != '.' && !stk.empty()) {
 				stk.pop();
 			}
 		}
-		if (!stb.toString().endsWith("/")) {
-			stb.append('/');
+		
+		String dots = "";
+		
+		while(!stk.isEmpty()) {
+			dots+=stk.pop();
 		}
-
+		
+		if(dots.length() > 2) {
+			dots = dots.substring(2,dots.length());
+		}
+		
+		//System.out.println(dots);
+		
+		if (!stb.toString().endsWith("/")) {
+			stb.append(dots).append('/');
+		}
+		
 		return stb.reverse().toString();
 
 	}
